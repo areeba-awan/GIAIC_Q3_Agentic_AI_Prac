975d06d8557d7658b0baf18fbef4ea843a916431
Areeba awan <156243202+areeba-awan@users.noreply.github.com>
2025-07-28 20:57:34 +0500

    Update Tracing.py

--- Streaming,Guardrails Prac/Tracing.py
+++ Streaming,Guardrails Prac/Tracing.py
@@ -1,10 +1,3 @@
-# class 06
-# GIthub : https://github.com/panaversity/learn-agentic-ai/tree/main/01_ai_agents_first/14_tracing
-
-# official docs
-# https://openai.github.io/openai-agents-python/tracing/
-
-# TRACING DASHBOARD LINK:  https://platform.openai.com/traces
 
 import os
 from agents import Agent, Runner, function_tool
@@ -25,6 +18,3 @@ simple_agent = Agent(
 
 result = Runner.run_sync(simple_agent, "What is the weather in karachi?",run_config = config)
 print("result >>>>> ",result.final_output)
-
-# Note : Tracing Agent ko Check krne k liye open ai ki api key buy krni paregi 
-# uske bina ye logs me record ni hoga iska kaam
